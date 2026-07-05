ffcf09bf8ec5596eb6327d5e440edf4cad8e996b
saturated-hombre <166325063+saturated-hombre@users.noreply.github.com>
2026-07-06 01:02:08 +0530

    Add Playround function logic

--- javascript.js
+++ javascript.js
@@ -19,3 +19,47 @@ function getHumanChoice(){
 let humanScore=0;
 let computerScore=0;
 
+function playRound(humanChoice,computerChoice){
+    //if humanChoice is equal to computerChoice then tie
+    if (humanChoice===computerChoice){
+        console.log('tie');
+    }
+    //if choice is rock vs scissor, rock wins, increments winner
+    else if((humanChoice=='rock' && computerChoice==='scissor')|| (humanChoice=='scissor' && computerChoice=='rock' )){
+        if(humanChoice=='rock'){
+            console.log('You win,Rock beats scissor');
+            humanScore++;
+        }
+        else{
+            console.log('You lose,Rock beats scissor');
+            computerScore++;
+        }
+    }
+    //if choice is rock vs paper, paper wins, increments winner
+    else if((humanChoice=='rock' && computerChoice==='paper')|| (humanChoice=='paper' && computerChoice=='rock' )){
+        if(humanChoice=='paper'){
+            console.log('You win, Paper beats rock');
+            humanScore++;
+        }
+        else{
+            console.log('You lose, Paper beats rock');
+            computerScore++;
+        }
+    }
+    //if choice is scissor vs paper, scissor wins
+    else if((humanChoice=='scissor' && computerChoice==='paper')|| (humanChoice=='paper' && computerChoice=='scissor' )){
+        if(humanChoice=='scissor'){
+            console.log('You win, scissor cuts paper ');
+            humanScore++;
+        }
+        else{
+            console.log('You lose, scissor cuts paper');
+            computerScore++;
+        }
+    }
+}
+
+const humanSelection=getHumanChoice();
+const computerSelection=getComputerChoice();
+
+playRound(humanSelection,computerSelection);
